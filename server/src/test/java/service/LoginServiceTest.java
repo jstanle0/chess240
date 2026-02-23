c73232e0c603d2edb1ad37c34144f3f4c3a6e26c
Joshua Stanley <jstanle0@byu.edu>
2026-02-23 14:51:12 -0700

    Login tests

--- server/src/test/java/service/LoginServiceTest.java
+++ server/src/test/java/service/LoginServiceTest.java
@@ -1,7 +1,9 @@
 package service;
 
 import io.javalin.http.ForbiddenResponse;
+import io.javalin.http.UnauthorizedResponse;
 import models.AuthData;
+import models.LoginUserData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -16,5 +18,19 @@ public class LoginServiceTest extends UnitTestUtils {
     @Test
     public void identicalRegisterTest() {
         Assertions.assertThrows(ForbiddenResponse.class, () -> LoginService.register(existingUser));
+    }
+
+    @Test
+    public void loginTest() {
+        LoginUserData loginData = new LoginUserData(existingUser.username(), existingUser.password());
+        AuthData authData = LoginService.login(loginData);
+        String username = Assertions.assertDoesNotThrow(() -> authDAO.getUsernameFromToken(authData.authToken()));
+        Assertions.assertEquals(username, existingUser.username());
+    }
+
+    @Test
+    public void invalidLoginTest() {
+        LoginUserData badLoginData = new LoginUserData(existingUser.username(), "fakePassword");
+        Assertions.assertThrows(UnauthorizedResponse.class, () -> LoginService.login(badLoginData));
     }
 }
