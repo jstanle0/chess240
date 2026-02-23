76d70e5fb12330d3294f77589b2510d5101a687a
Joshua Stanley <jstanle0@byu.edu>
2026-02-23 15:02:11 -0700

    Logout tests

--- server/src/test/java/service/LoginServiceTest.java
+++ server/src/test/java/service/LoginServiceTest.java
@@ -1,11 +1,14 @@
 package service;
 
+import dataaccess.DataAccessException;
 import io.javalin.http.ForbiddenResponse;
 import io.javalin.http.UnauthorizedResponse;
 import models.AuthData;
 import models.LoginUserData;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
 
 public class LoginServiceTest extends UnitTestUtils {
     @Test
@@ -32,5 +35,22 @@ public class LoginServiceTest extends UnitTestUtils {
     public void invalidLoginTest() {
         LoginUserData badLoginData = new LoginUserData(existingUser.username(), "fakePassword");
         Assertions.assertThrows(UnauthorizedResponse.class, () -> LoginService.login(badLoginData));
+    }
+
+    @Test
+    public void logoutTest() {
+        Assertions.assertDoesNotThrow(() -> LoginService.logout(existingAuthToken));
+        Assertions.assertThrows(DataAccessException.class, () -> authDAO.getUsernameFromToken(existingAuthToken));
+    }
+
+    @Test
+    public void invalidLogoutTest() {
+        Assertions.assertThrows(UnauthorizedResponse.class, () -> LoginService.logout(UUID.randomUUID()));
+    }
+
+    @Test
+    public void verifyAuthTest() {
+        Assertions.assertDoesNotThrow(() -> LoginService.verifyAuth(existingAuthToken));
+        Assertions.assertThrows(UnauthorizedResponse.class, () -> LoginService.verifyAuth(UUID.randomUUID()));
     }
 }
